05807b64032232070381148dfd99ebf7f3585274
nk__9512 <sylph.jump@gmail.com>
2024-02-25 02:16:47 +0900

    シフトレジスタ仮完成

--- ShiftRegister/src/main.cpp
+++ ShiftRegister/src/main.cpp
@@ -3,13 +3,26 @@
 const uint8_t rclkPin  = 11;   // (11) ST_CP [RCLK] on 74HC595
 const uint8_t srclkPin =  9;  // (9)  SH_CP [SRCLK] on 74HC595
 const uint8_t dsPin = 12;     // (12) DS [SER] on 74HC595
-const uint8_t tDelay = 200;   //200ms遅延設定
+const uint8_t tDelay = 10;   //200ms遅延設定
 
 //Lesson 20-1 8bitシフトレジスタを使ってLEDを順番に点灯２
 //https://omoroya.com/
 
+int Position[2] = {0};
+uint8_t leds[8] = {B00000000}; //ledsをuint8_t型配列としてb00000000で定義
 
-byte leds = B00000000; //ledsをbyte型としてb00000000で定義
+void BitTranslate(int Position[],uint8_t bitData[])  //Position...センサーの位置の要素数2の配列
+{
+  uint8_t ForShift = B00000001;
+  bitData[Position[0]] |= (ForShift >> Position[1]);
+}
+
+void ToShiftRegister(uint8_t bitData[]){
+  for (int i = 0; i < 8; i++)
+  {
+      shiftOut(dsPin, srclkPin, LSBFIRST, bitData[i]);
+  }
+}
 
 void setup() 
 {
@@ -20,24 +33,10 @@ void setup()
 
 void loop() 
 {
-  //　LED1からLED8までのレジスタを初期化。
-  leds = B11111111;                          //初期化設定、b11111111（全て消灯）
+  BitTranslate(Position,leds);
   digitalWrite(rclkPin, LOW);                //送信中のRCLKをLowにする
-  shiftOut(dsPin, srclkPin, LSBFIRST, leds); //全てのLEDを消灯
+  ToShiftRegister(leds);
   digitalWrite(rclkPin, HIGH);               //送信終了後RCLKをHighにする
   
   delay(tDelay);
-  
-  //　LED1からLED8までを順に光らせる。
-  for (int i = 0; i < 8; i++)
-  {
-    bitClear(leds, i);                           //bitbyte操作関数で指定したビットを0にする
-    
-    digitalWrite(rclkPin, LOW);                //送信中のRCLKをLowにする
-    shiftOut(dsPin, srclkPin, LSBFIRST, leds); //シフト演算を使って点灯するLEDを選択
-    digitalWrite(rclkPin, HIGH);               //送信終了後RCLKをHighにする
-    
-    delay(tDelay);
-  }
 }
-//うんち
