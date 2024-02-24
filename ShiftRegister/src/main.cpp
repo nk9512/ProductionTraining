2a0d4ee793ea7311c393d14428b389de60a8e148
nk__9512 <sylph.jump@gmail.com>
2024-02-25 03:42:38 +0900

    右shiftと左shiftミスってた

--- ShiftRegister/src/main.cpp
+++ ShiftRegister/src/main.cpp
@@ -8,19 +8,20 @@ const uint8_t tDelay = 10;   //200ms遅延設定
 //Lesson 20-1 8bitシフトレジスタを使ってLEDを順番に点灯２
 //https://omoroya.com/
 
-int Position[2] = {0};
+int Position[2] = {0};  //Position...センサーの位置の要素数2の配列
 uint8_t leds[8] = {B00000000}; //ledsをuint8_t型配列としてb00000000で定義
 
-void BitTranslate(int Position[],uint8_t bitData[])  //Position...センサーの位置の要素数2の配列
+void BitTranslate(int Position[],uint8_t bitData[])   //2要素配列の位置データをビット単位に変換
 {
   uint8_t ForShift = B00000001;
-  bitData[Position[0]] |= (ForShift >> Position[1]);
+  bitData[Position[0]] |= (ForShift << Position[1]);
 }
 
-void ToShiftRegister(uint8_t bitData[]){
+void ToShiftRegister(uint8_t bitData[]) //シフトレジスターにLED点灯位置のデータを送信
+{
   for (int i = 0; i < 8; i++)
   {
-      shiftOut(dsPin, srclkPin, LSBFIRST, bitData[i]);
+      shiftOut(dsPin, srclkPin, LSBFIRST, bitData[i]);  //ShiftOut...Byte単位でデータをシリアル的に送信する。
   }
 }
 
